c8a32c6b152cb86406691590a2fd6dd2ad980faa
Ernest Kan <ek96122@gmail.com>
2016-03-23 19:09:58 +0000

    Initial Commit

---
+++ HPC q3(c)(d)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HPC q3(c)(d)
+//
+//  Created by Ernest on 23/3/2016.
+//  Copyright © 2016 Kan Tsz Hang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
