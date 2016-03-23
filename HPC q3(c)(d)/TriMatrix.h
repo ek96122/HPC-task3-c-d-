f1a6ffb70a77b1615abd9a6e05afa73a5b1b7013
Ernest Kan <ek96122@gmail.com>
2016-03-23 19:11:31 +0000

    start

---
+++ HPC q3(c)(d)/TriMatrix.h
@@ -0,0 +1,13 @@
+//
+//  TriMatrix.h
+//  HPC q3(c)(d)
+//
+//  Created by Ernest on 23/3/2016.
+//  Copyright © 2016 Kan Tsz Hang. All rights reserved.
+//
+
+#ifndef TriMatrix_h
+#define TriMatrix_h
+
+
+#endif /* TriMatrix_h */
