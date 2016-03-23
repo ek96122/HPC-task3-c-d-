5db617e83e445ba6152e7fa7ba4a7f8b51845aa3
Ernest Kan <ek96122@gmail.com>
2016-03-23 19:55:26 +0000

    trial 1

--- HPC q3(c)(d)/main.cpp
+++ HPC q3(c)(d)/main.cpp
@@ -7,9 +7,90 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "TriMatrix.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+    double L, T, alpha, dt, theta;
+    int Nx;
+    
+    
+    // get user input
+    {using namespace std;
+        cout << "L: ";
+        cin >> L;
+        
+        cout << "T: ";
+        cin >> T;
+        
+        cout << "alpha: ";
+        cin >> alpha;
+        
+        cout << "Nx: ";
+        cin >> Nx;
+        
+        cout << "dt: ";
+        cin >> dt;
+    
+        cout << "theta (0 forward Euler, 0.5 Crank-Nicolson, 1 backward Euler):";
+        cin >> theta;
+    }
+    
+    double dx = L/Nx;
+    double nu = alpha*dt/dx/dx;
+    double gamma0 = 0;
+    double gamma1 = 0;
+    
+    
+    //Defining all BASIC vectors used in the programme
+    
+    vector<double> x((Nx+1));
+    vector<double> u0((Nx+1));
+    vector<double> u1(Nx+1);
+    vector<double> u2(Nx+1);
+    
+    
+    //Defining x-vector
+    for (int i=0;i<Nx+1;i++) {
+        x[i]=dx*i; //equallly spaced elements in x-vector
+    }
+    
+    //Defining a vector, which uses the initial condition u0, to hold the solution
+    for (int i=0; i<Nx+1; i++) {
+        u0[i]=x[i]/(1-x[i]);
+    }
+    
+    //Transfer entries from u0 vector to the u1 vector created earlier
+    
+    
+    for (int i=0; i<Nx+1; i++) {
+        
+        (u1)[i] = u0[i];
+    }
+    
+    u1[0]=gamma0;
+    u1[Nx]=gamma1;
+   
+    
+    //create tridiagonal matrix using TriMatrix class constructor
+    
+    TriMatrix LH(Nx, theta * nu);
+    TriMatrix RH(Nx, (1-theta) * nu);
+    
+
+    LH.displayM();
+    RH.displayM();
+    
+    for (double i=0; i<(T-dt); i=i+dt) {
+        u2=LH/RH.multi(u1);
+        u1=u2;
+    }
+    
+    
+    
     return 0;
+    
+    
 }
